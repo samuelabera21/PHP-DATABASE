47fce273892bcbdd702fe2bcdb2efcdc236114dd
Samuel Abera <sami21.good.bad@gmail.com>
2026-05-17 11:27:23 +0300

    project  form

---
+++ project.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project page</title>
+</head>
+
+<body>
+    <h2> project page</h2>
+    <form action="insert_project.php" method="POST">
+        <label for="student_id">student id</label>
+        <input type="number" , id="student_id" , name="student_id" , required><br><br>
+        <label for="hourly_rate">hourly rate</label>
+        <input type="number" , id="hourly_rate" , name="hourly_rate" , required><br><br>
+        <lable for="project_name">project name</lable>
+        <input type="text" , id="project_name" , name="project_name" , required><br><br>
+        <button type="submit">submit</button>
+    </form>
+
+
+
+    </form>
+</body>
+
+</html>
