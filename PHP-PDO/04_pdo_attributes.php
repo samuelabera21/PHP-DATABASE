1d968db6e8de238b3c5a08948b38a0cbb446c6ec
Samuel Abera <sami21.good.bad@gmail.com>
2026-06-05 10:42:59 +0300

    Add 04_pdo_attributes

---
+++ PHP-PDO/04_pdo_attributes.php
@@ -0,0 +1,6 @@
+<?php
+$pdo = new PDO('sqlite::memory:');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+echo 'PDO attributes configured';
